9c8d2a4001c4191f0591e955e16c7bb2e4a455a6
KaterynaKucherenko <estorskaya@gmail.com>
2023-11-08 13:26:25 +0200

    delete deprecated API

--- module-main/src/main/java/com/mjc/school/main/AppConfiguration.java
+++ module-main/src/main/java/com/mjc/school/main/AppConfiguration.java
@@ -5,9 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@ComponentScan("com.mjc.school.*")
+@ComponentScan("com.mjc.school")
 @EnableAspectJAutoProxy
 public class AppConfiguration {
-
 }
 
