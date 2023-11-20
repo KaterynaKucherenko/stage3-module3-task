700113db689bb71641bd6d44a9478a6227b4fd71
KaterynaKucherenko <estorskaya@gmail.com>
2023-11-20 19:07:40 +0200

    impl some depend

--- module-main/src/main/java/com/mjc/school/main/Main.java
+++ module-main/src/main/java/com/mjc/school/main/Main.java
@@ -1,14 +1,20 @@
 package com.mjc.school.main;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 import java.lang.reflect.InvocationTargetException;
-
+@SpringBootApplication
+@EnableAspectJAutoProxy
+@EnableJpaAuditing
 public class Main {
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+        ApplicationContext applicationContext = SpringApplication.run(Main.class, args);
         Menu menu = applicationContext.getBean(Menu.class);
         menu.start();
     }
